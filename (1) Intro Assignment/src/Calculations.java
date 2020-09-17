14bde9b34fd6cb680b0229a74645c7905764b3d1
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-09-16 17:05:32 -0700

    asdf

--- (1) Intro Assignment/src/Calculations.java
+++ (1) Intro Assignment/src/Calculations.java
@@ -1,81 +1,133 @@
+import com.sun.deploy.security.SelectableSecurityManager;
+
 import java.sql.SQLOutput;
 
 public class Calculations {
-    int x;
-    int a;
-    int b;
-    int c;
-    int d;
-    int w;
-    int j;
+    double x;
+    double ex;
+    double a;
+    double b;
+    double c;
+    double d;
+    boolean tex;
+    boolean tx;
 
-    public int getX() {
-        return x;
-    }
-    public void setX(int x) {
-        this.x = x;
-    }
-    public int getA() {
-        return a;
-    }
-    public void setA(int a) {
-        this.a = a;
-    }
-    public int getB() {
-        return b;
-    }
-    public void setB(int b) {
-        this.b = b;
-    }
-    public int getC() {
-        return c;
-    }
-    public void setC(int c) {
-        this.c = c;
-    }
-
-    Calculations(int a, int b, int c){
+    Calculations(double a, double b, double c){
         this.a = a;
         this.b = b;
         this.c = c;
     }
 
-    Calculations(int a, int b, int c, int d){
+    Calculations(double a, double b, double c, double d){
         this.a = a;
         this.b = b;
         this.c = c;
         this.d = d;
     }
 
-    public int questionOne(){
+
+    public String questionOne(){
         System.out.println("Question 1: ");
         System.out.println("ax + b = c");
-        System.out.printf("%dx + %d = %d" , a, b, c);
-        int w = c-b;
+        System.out.printf("%fx + %f = %f" , a, b, c);
+        double w = c-b;
         System.out.println();
-        System.out.printf("%dx = %d" , a, w);
+        System.out.printf("%fx = %f" , a, w);
         System.out.println();
-        int j = w/a;
-        System.out.println("x = " + j);
-        System.out.println("ANSWER:");
-        return j;
+        double x = w/a;
+        System.out.println("x = " + x);
+        return "";
     }
 
-    public int questionTwo(){
+    public String questionTwo(){
         System.out.println("Question 2:");
         System.out.println("ax^2 + bx + c = d");
         System.out.println(a + "x^2 + " + b + "x + " + c + " = " + d);
-        c = c-d;
-        System.out.println(a + "x^2 + " + b + "x + " + c + " = " + 0);
+        double cee = c-d;
+        System.out.println(a + "x^2 + " + b + "x + " + cee + " = " + 0);
+        System.out.printf("\na = %.3f, b = %.3f, c = %.3f\n", a, b, cee);
+        //quadratic equation starts here
+        System.out.println("x = (-b\u00B1\u221Ab^2-4ac)/2a");
+        System.out.printf("x = [-%.3f\u00B1\u221A%.3f^2-4(%.3f)(%.3f)]/2(%.3f)\n", b, b, a, cee, a);
+        double bb = b*b;
+        double ac = -4*a*cee;
+        double denom = 2*a;
+        double bn = b*-1;
+        System.out.printf("x = (%.3f\u00B1\u221A%.3f+%.3f)/%.3f\n", bn, bb, ac, denom);
+        double bbac = bb+ac;
+        System.out.printf("x = (%.3f\u00B1\u221A%.3f)/%.3f\n", bn, bbac, denom);
+        System.out.printf("x = (%.3f+\u221A%.3f)/%.3f, (%.3f-\u221A%.3f)/%.3f\n", bn, bbac, denom, bn, bbac, denom);
+        ex = x;
+        x = bn - Math.sqrt(bbac);
+        x = x/denom;
+        ex = bn + Math.sqrt(bbac);
+        ex = ex/denom;
+
+//        System.out.println("ax^2 + bx + c = d");
+ //       System.out.printf("%d(%.1f)(%.1f) + %d(%.1f) + %d = %d\n", a, x, x, b, x, c, d);
+        double axx = a*x*x;
+        double bx = b*x;
+  //      System.out.printf("%.1f + %.1f + %d = %d\n", axx, bx, c, d);
+        double ans = axx+bx+c;
+//        System.out.println("ax^2 + bx + c = d");
+        //System.out.printf("%d(%.1f)(%.1f) + %d(%.1f) + %d = %d\n", a, ex, ex, b, ex, c, d);
+        double axxe = a*ex*ex;
+        double bxe = b*ex;
+        //System.out.printf("%.1f + %.1f + %d = %d\n", axx, bx, c, d);
+        double anse = axxe+bxe+c;
+        tx = ans == d;
+        tex = anse == d;
 
 
+        if(tx && tex){
+            System.out.printf("x = %.3f, %.3f", x, ex);
+        }
+        else if(tx){
+                System.out.printf("x = %.3f", x);
+        }
+        else if (tex){
+            System.out.printf("x = %.3f", ex);
+        }
+        else {
+            System.out.println("NO ANSWER");
+        }
 
-        System.out.println();
-        System.out.println();
-        return w;
+        return "";
     }
 
-    public String toString(){
-        return "x = " + x;
+    public String questionThree(){
+        System.out.println("Question 3: ");
+        System.out.printf("Points: (%.1f, %.1f), (%.1f, %.1f)\n", a, b, c, d);
+        double up = Math.abs(d-b);
+        double side = Math.abs(c-a);
+        System.out.println("a^2 + b^2 = c^2\n");
+        System.out.printf("%.1f^2 + %.1f^2 = c^2\n", up, side);
+        double squp = up*up;
+        double sqside = side*side;
+        System.out.printf("%.1f + %.1f = c^2\n", squp, sqside);
+        double tot = squp+sqside;
+        System.out.printf("%.1f = c^2\n", tot);
+        double ans = Math.sqrt(tot);
+        System.out.printf("%.2f = c\n\n", ans);
+        return "";
     }
+
+    public String questionFour(){
+        System.out.println("Question 4: ");
+        System.out.printf("Points: (%.1f, %.1f), (%.1f, %.1f)\n", a, b, c, d);
+        double up = Math.abs(d-b);
+        double side = Math.abs(c-a);
+        System.out.println("a^2 + b^2 = c^2\n");
+        System.out.printf("%.1f^2 + %.1f^2 = c^2\n", up, side);
+        double squp = up*up;
+        double sqside = side*side;
+        System.out.printf("%.1f + %.1f = c^2\n", squp, sqside);
+        double tot = squp+sqside;
+        System.out.printf("%.1f = c^2\n", tot);
+        double ans = Math.sqrt(tot);
+        System.out.printf("%.2f = c\n\n", ans);
+        return "";
+    }
+
+
 }
--- (1) Intro Assignment/src/Main.java
+++ (1) Intro Assignment/src/Main.java
@@ -3,35 +3,53 @@ import java.util.Scanner;
 // 2) Solve for 'x' with an expression of ax2+bx+c=d where a, b, c, d are constants
 // 3) given 2 coordinates calculate the distance between the 2 spots on a cartesian plane
 // 4) given 2 coordinates calculate the direction in degrees or radians, specify which in comment
-// 2, 3, 4 unfinished, 1 finished
-//2 is quadratic equation? ask how to do it later
+// 3, 4 unfinished, 1, 2 finished
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Question 1, 2, 3, or 4?");
-        int question = scan.nextInt();
+        //int question = scan.nextInt();
+        int question = 4;
         if (question == 1){
             System.out.println("Formula is ax + b = c");
             System.out.println("What are 'a', 'b', and 'c'?");
-            int ay = scan.nextInt();
-            int bee = scan.nextInt();
-            int see = scan.nextInt();
+            double ay = scan.nextDouble();
+            double bee = scan.nextDouble();
+            double see = scan.nextDouble();
             Calculations findXone = new Calculations(ay, bee, see);
             System.out.println(findXone.questionOne());
         }
 
         else if (question == 2){
             System.out.println("Formula is ax^2 + bx + c = d");
-            int ay = 3;
-            int bee = 3;
-            int see = 2;
-            int dee = 20;
+            System.out.println("What are 'a', 'b', 'c', and 'd'?");
+            double ay = scan.nextDouble();
+            double bee = scan.nextDouble();
+            double see = scan.nextDouble();
+            double dee = scan.nextDouble();
             Calculations findXtwo = new Calculations(ay, bee, see, dee);
             System.out.println(findXtwo.questionTwo());
         }
 
-
+        else if (question == 3){
+            System.out.println("What are your points? (a,b), (c,d)");
+            double ay = scan.nextDouble();
+            double bee = scan.nextDouble();
+            double see = scan.nextDouble();
+            double dee = scan.nextDouble();
+            Calculations findXthree = new Calculations(ay, bee, see, dee);
+            System.out.println(findXthree.questionThree());
+        }
+        else if (question == 4){
+            System.out.println("What are your points? (a,b), (c,d)");
+            double ay = 0;
+            double bee = 0;
+            double see = 0;
+            double dee = 0;
+            Calculations findXfour = new Calculations(ay, bee, see, dee);
+            System.out.println(findXfour.questionFour());
+        }
 
 
     }
