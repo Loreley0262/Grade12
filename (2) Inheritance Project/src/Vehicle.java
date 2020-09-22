91ed2aa0b3dd16f4f462d4d84efd765d4460dbb9
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-09-22 14:26:36 -0700

    asdf

---
+++ (2) Inheritance Project/src/Vehicle.java
@@ -0,0 +1,15 @@
+public class Vehicle {
+    public int registrationNumber;
+    public Person owner;
+
+    Vehicle(int registrationNumber, Person owner){
+        this.registrationNumber = registrationNumber;
+        this.owner = owner;
+    }
+
+    Vehicle(){
+        registrationNumber = 0;
+        owner = null;
+    }
+
+}
