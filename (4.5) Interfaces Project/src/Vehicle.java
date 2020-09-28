5c12bf6ddd3d814ddcc3f3e6df1b1c99ff6092ca
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-09-28 13:24:55 -0700

    asdf

---
+++ (4.5) Interfaces Project/src/Vehicle.java
@@ -0,0 +1,16 @@
+public  interface Vehicle {
+
+
+
+
+// all are the abstract methods.
+
+        void changeGear(int a);
+
+        void speedUp(int a);
+
+        void applyBrakes(int a);
+
+
+
+}
