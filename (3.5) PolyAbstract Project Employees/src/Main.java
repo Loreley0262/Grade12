790c629eb5c64a8a1a4ba3ca56bbbabf9d9f8523
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-09-28 14:47:33 -0700

    asdf

--- (3.5) PolyAbstract Project Employees/src/Main.java
+++ (3.5) PolyAbstract Project Employees/src/Main.java
@@ -3,10 +3,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Employee> employeeList = new ArrayList<>();
         ArrayList<PartTime> partTimeEmployeeList = new ArrayList<>();
+        ArrayList<FullTime> fullTimeEmployeeList = new ArrayList<>();
         ArrayList<SalaryEmployee> salaryEmployeeList = new ArrayList<>();
 
-        PartTime bob = new PartTime("EMPBob", 2000, 15.5, 2000) {
+        employeeList.add(new PartTime("PartOne", 2000, 15.5, 2000) {
             @Override
             public boolean hasBenefits() {
                 return false;
@@ -21,16 +23,44 @@ public class Main {
             public double monthlySalary() {
                 return 0;
             }
-        };
-        PartTime joe = new PartTime("PTJoe", 25.5, 14.60, 2000);
-        SalaryEmployee sal = new SalaryEmployee("Sal", 2000, 5000);
+        });
+        employeeList.add(new PartTime("PartTwo", 2000, 15.5, 2000) {
+            @Override
+            public boolean hasBenefits() {
+                return false;
+            }
 
-        partTimeEmployeeList.add(bob);
-        partTimeEmployeeList.add(joe);
+            @Override
+            public double annualSalary() {
+                return 0;
+            }
 
-        salaryEmployeeList.add(sal);
+            @Override
+            public double monthlySalary() {
+                return 0;
+            }
+        });
+        employeeList.add(new FullTime("FullOne", 25.5, 14.60, 2000));
+        employeeList.add(new FullTime("FullTwo", 25.5, 14.60, 2000));
+        employeeList.add(new SalaryEmployee("SalOne", 2000, 5000));
+        employeeList.add(new SalaryEmployee("SalTwo", 2000, 5000));
 
-        System.out.println(salaryEmployeeList + "\n" + partTimeEmployeeList);
+        for (Employee emp : employeeList){
+            if(emp instanceof PartTime){
+                partTimeEmployeeList.add((PartTime) emp);
+            }
+            else if(emp instanceof FullTime){
+                fullTimeEmployeeList.add((FullTime) emp);
+            }
+            else if(emp instanceof SalaryEmployee){
+                salaryEmployeeList.add((SalaryEmployee) emp);
+            }
+        }
+
+        System.out.println("List of all Employees:\n" + employeeList);
+        System.out.println("\nList of Part Time Employees:\n" + partTimeEmployeeList);
+        System.out.println("\nList of Full Time Employees:\n" + fullTimeEmployeeList);
+        System.out.println("\nList of Salary Employees:\n" + salaryEmployeeList);
 
     }
 }
