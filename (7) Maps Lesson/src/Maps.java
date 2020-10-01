41637ad863f049bb92a64f1e1f1df8d50e43ae28
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-10-01 14:54:13 -0700

    asdf

---
+++ (7) Maps Lesson/src/Maps.java
@@ -0,0 +1,10 @@
+public class Maps {
+    /*not in collections interface, diff than sets/lists
+    have key and value
+        key "points" to value
+        keys are unique, values aren't always unique
+    ex: HashMap, HashTable, LinkedHashMap and treeMap constructs
+        HashMap/HashTable
+
+    */
+}
