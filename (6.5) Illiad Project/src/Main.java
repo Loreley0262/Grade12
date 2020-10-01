30f5f1481e9b20828b9076ce9580ef937448ef1f
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-10-01 14:29:08 -0700

    asfd

--- (6.5) Illiad Project/src/Main.java
+++ (6.5) Illiad Project/src/Main.java
@@ -1,31 +1,43 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(new File("illiad.txt"));
-
         TreeSet<String> ilYA = new TreeSet<>();
-        for (int i = 1; i<10; i++){
-            ilYA.add("e");
-            ilYA.add("f");
+        ArrayList<String> totalNumWords = new ArrayList<>();
+        String word = "";
+        String test = "\"Sons of Atreus,\" he cried, \"and all other Achaeans, may the gods\n" +
+                "who dwell in Olympus grant you to sack the city of Priam, and to reach\n" +
+                "your homes in safety; but free my daughter, and accept a ransom for\n" +
+                "her, in reverence to Apollo, son of Jove.\" ";
+
+        while (scan.hasNext()) {
+            word = scan.next();
+            word = word.toLowerCase();
+            totalNumWords.add(word);
+            ilYA.add(word);
+            System.out.print(word + "  ");
         }
-        System.out.println(ilYA);
 
-        TreeSet<Ileeahd> illeeaad = new TreeSet<>();
-        Ileeahd ileeahd = new Ileeahd("as");
-        illeeaad.add(ileeahd);
-        System.out.println(ileeahd);
+        String[] totalNumWordsArray = totalNumWords.toArray(new String[0]);
+        String[] ilYArray = ilYA.toArray(new String[0]);
+//        String[] testArray = test.toArray(new String[0]);
+        int origNum = 0;
+        int sortNum = 0;
 
-        Ileeahd x = new Ileeahd("aa");
-        Ileeahd y = new Ileeahd("aa");
-
-
-        for (int i = 1; i<4; i++){
-            String a = scan.next();
-            System.out.println(a);
+        for (String s : totalNumWordsArray) {
+            origNum++;
         }
+        for (String s : ilYArray) {
+            sortNum++;
+        }
+        System.out.println(origNum);
+        System.out.println(sortNum);
+
+
     }
 }
