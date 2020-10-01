1e7c091da8f84e9609a692c4f89815d08fbb4fb8
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-10-01 14:31:39 -0700

    asdf

--- (6.5) Illiad Project/src/Main.java
+++ (6.5) Illiad Project/src/Main.java
@@ -7,21 +7,27 @@ import java.util.TreeSet;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(new File("illiad.txt"));
+        Scanner scan2 = new Scanner(new File("illiad test.txt"));
+
         TreeSet<String> ilYA = new TreeSet<>();
         ArrayList<String> totalNumWords = new ArrayList<>();
         String word = "";
-        String test = "\"Sons of Atreus,\" he cried, \"and all other Achaeans, may the gods\n" +
-                "who dwell in Olympus grant you to sack the city of Priam, and to reach\n" +
-                "your homes in safety; but free my daughter, and accept a ransom for\n" +
-                "her, in reverence to Apollo, son of Jove.\" ";
 
-        while (scan.hasNext()) {
+        while (scan2.hasNext()){
             word = scan.next();
             word = word.toLowerCase();
             totalNumWords.add(word);
             ilYA.add(word);
             System.out.print(word + "  ");
         }
+
+//        while (scan.hasNext()) {
+//            word = scan.next();
+//            word = word.toLowerCase();
+//            totalNumWords.add(word);
+//            ilYA.add(word);
+//            System.out.print(word + "  ");
+//        }
 
         String[] totalNumWordsArray = totalNumWords.toArray(new String[0]);
         String[] ilYArray = ilYA.toArray(new String[0]);
