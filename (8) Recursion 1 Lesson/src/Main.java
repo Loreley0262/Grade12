b1f31d27ee32dbec3f3ed9eaa38baf1610402816
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-10-19 14:12:41 -0700

    asdf

---
+++ (8) Recursion 1 Lesson/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    /*
+    this method multiplies a and b together
+    kinda like multirec but simpler with only loops and no recursion
+    * */
+    public static int multIter(int a, int b){
+        int result = 0;
+        for (int i = 0; i<b; i++){
+            result +=a;
+        }
+        return result;
+    }
+
+    /*this is recursion
+    ////recursion is a method that calls on itself////
+    * must have one or more base cases to solve, prevent infinite recursion
+    *   infinite recursion goes on forever, is bad
+    * base cases are easy to solve cases [ex. if (b==0) return 1;]
+    *   simple cases that can be solved directly
+    * kinda like loops but more
+    * */
+
+    public static int multiRec(int a, int b){
+        int result = 0;
+        //base case
+        if(b==1){
+            return a;
+        }
+        //recursive step
+        result = a+multiRec(a, b-1);
+        return result;
+    }
+
+    //finding n! with recursion
+    public static int factorial(int n){
+        if (n==1){
+            return 1;
+        }
+        else{
+            return n*factorial(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(multIter(8,5));
+        System.out.println(multiRec(8,5));
+        System.out.println(factorial(4));
+    }
+}
