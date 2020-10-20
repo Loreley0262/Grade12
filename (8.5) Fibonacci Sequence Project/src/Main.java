cbb456f9d6522b8a0345a48e1f700ce8c0a355d4
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-10-20 15:02:26 -0700

    asdf

--- (8.5) Fibonacci Sequence Project/src/Main.java
+++ (8.5) Fibonacci Sequence Project/src/Main.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-
+    static int n1 = 0;
+    static int n2 = 1;
+    static int n3 = 0;
     public static int fibo(int n){
         int result;
         //base case
-        if(n==1||n==2){
-            return 1;
+        if (n<=0){
+            return 0;
         }
         //recursive step
-        result = fibo(n-1) + fibo(n-2);
-        return result;
+        else{
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            System.out.print(" "+n3);
+            fibo(n-1);
+        }
+        return n3;
     }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("index of num in sequence to be printed");
-        int fibothNum = scan.nextInt();
-        System.out.println(fibo(fibothNum));
+        System.out.println("how many nums in sequence should be shown?");
+        int fiboNums = 6;
+        fibo(fiboNums);
     }
 }
