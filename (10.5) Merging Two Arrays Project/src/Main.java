177dea75bec93fba41159162e3a2415524514eb2
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-10-21 15:02:42 -0700

    asdf

--- (10.5) Merging Two Arrays Project/src/Main.java
+++ (10.5) Merging Two Arrays Project/src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    /*
     public static void getLeafNodes(Node n){
         //base case
         if (n.left == 0 && n.right == 0){
@@ -19,8 +20,46 @@ public class Main {
         }
 
     }
-    int[] ay = new int[8];
+    */
 
+    public static void main(String[] args) {
+    /*    int[] ay = new int[6];
+        int[] bee = new int[9];
+        int[] merged = new int[ay.length + bee.length];
 
+        ay[0] = 27;             bee[0] = 84;
+        ay[1] = 24;             bee[1] = 81;
+        ay[2] = 23;             bee[2] = 35;
+        ay[3] = 15;             bee[3] = 25;
+        ay[4] = 10;             bee[4] = 14;
+        ay[5] = 8;              bee[5] = 11;
+                                bee[6] = 8;
+                                bee[7] = 5;
+                                bee[8] = 2;
 
+        for (int i = 0; i< ay.length; i++){
+            System.out.println(ay[i]);
+        }
+        System.out.println();
+        System.out.println();
+        for (int i = 0; i< bee.length; i++){
+            System.out.println(bee[i]);
+        }
+    */
+        
+    }
 }
+/*
+    a       b
+    27      84
+    24      81
+    23      35
+    15      25
+    10      14
+    8       11
+            8
+            5
+            2
+
+
+*/
