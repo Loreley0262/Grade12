8bd5588c23f51b7d6965c5d08f19a03694629faa
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-10-30 15:02:51 -0700

    asdf

---
+++ (14.5) WindowsLesson Notes my copy/src/Layout/Delmember.java
@@ -0,0 +1,59 @@
+package Layout;
+
+import Database.DatabaseHandler;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Delmember implements Initializable {
+    public TextField getId;
+    public TextField getName;
+    public TextField getEmail;
+    public TextField getNick;
+    public DatabaseHandler handler;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        handler = new DatabaseHandler();
+    }
+
+
+    public void delMember(ActionEvent actionEvent) {
+        String n = getName.getText();
+        String i = getId.getText();
+        String email = getEmail.getText();
+        String nick = getNick.getText();
+
+        boolean flag = n.isEmpty() || i.isEmpty() || email.isEmpty() || nick.isEmpty();
+        if (flag){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Please enter all fields");
+            alert.setHeaderText(null);
+            alert.showAndWait();
+            return;
+        }
+        String st = "INSERT INTO MEMBER VALUES (" +
+                "'" + i + "'," +
+                "'" + n + "'," +
+                "'" + email + "'," +
+                "'" + nick + "'" + ")";
+        System.out.println(st);
+        if(handler.execAction(st)){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("Deleted successfully");
+            alert.setHeaderText(null);
+            alert.showAndWait();
+        }
+        else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Error with deleting data");
+            alert.setHeaderText(null);
+            alert.showAndWait();
+        }
+    }
+}
--- (14.5) WindowsLesson Notes my copy/src/sample/Controller.java
+++ (14.5) WindowsLesson Notes my copy/src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
 public class Controller {
+
+
+     void loadWindow (String location, String title){
+         try{
+             Parent parent = FXMLLoader.load(getClass().getResource(location));
+             Stage stage = new Stage(StageStyle.DECORATED);
+             stage.setTitle(title);
+             stage.setScene(new Scene(parent));
+             stage.show();
+         }
+         catch(IOException e){
+             e.printStackTrace();
+         }
+     }
+
+    public void addMemberWindow(javafx.event.ActionEvent actionEvent) {
+        loadWindow("../Layout/addmember.fxml", "Add Member");
+    }
+
+    public void delMemberWindow(ActionEvent actionEvent) {
+         loadWindow("../Layout/delmember.fxml", "Del Member");
+    }
 }
